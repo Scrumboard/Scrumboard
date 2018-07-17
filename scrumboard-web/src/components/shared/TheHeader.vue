8e9956c805fa80b71f984f7f95ca2cb8ca1a9d7c
Erwin Olie <erwinolie@gmail.com>
2018-07-17 21:13:34 +0200

    refactored TheHeader

--- scrumboard-web/src/components/shared/TheHeader.vue
+++ scrumboard-web/src/components/shared/TheHeader.vue
@@ -1,103 +1,48 @@
 <template>
-  <div class="pos-f-t">
+  <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
 
-  <nav class="navbar navbar-dark bg-dark">
-    <a class="navbar-brand">Scrumboard</a>
+    <a class="navbar-brand" href="#">Scrumboard</a>
 
-    <ul class="navbar-nav ml-auto d-flex flex-sm-row dissapear-small">
-      <li class="nav-item mr-4 pl-2 pr-2">
-        <router-link :to="{ name: 'login' }" class="nav-link">Login</router-link>
-      </li>
-      <li class="nav-item mr-4 pl-2 pr-2">
-        <a class="nav-link" href="#">Register</a>
-      </li>
-      <li class="nav-item mr-4 pl-2 pr-2">
-        <router-link :to="{ name: 'Landing' }" class="nav-link">Home</router-link>
-      </li>
-    </ul>
-
-    <!-- The if statement should be connected to logged in or not. -->
-    <button class="navbar-toggler align-right" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
       <span class="navbar-toggler-icon"></span>
     </button>
-  </nav>
-
-    <div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-dark p-2">
-    <ul class="navbar-nav ml-auto d-flex flex-sm-row">
-      <li class="nav-item mr-4 pl-2 pr-2">
-        <router-link :to="{ name: 'login' }" class="nav-link">Login</router-link>
-      </li>
-      <li class="nav-item mr-4 pl-2 pr-2">
-        <a class="nav-link" href="#">Register</a>
-      </li>
-      <li class="nav-item mr-4 pl-2 pr-2">
-        <router-link :to="{ name: 'Landing' }" class="nav-link">Home</router-link>
-      </li>
-    </ul>
+    <div class="collapse navbar-collapse" id="navbar">
+      <ul class="navbar-nav ml-auto text-center">
+        <li class="nav-item px-3">
+          <router-link :to="{ name: 'login' }" class="nav-link">Login</router-link>
+        </li>
+        <li class="nav-item px-3">
+          <router-link :to="{ name: 'register' }" class="nav-link">Register</router-link>
+        </li>
+        <li class="nav-item px-3">
+          <router-link :to="{ name: 'Landing' }" class="nav-link">Home</router-link>
+        </li>
+      </ul>
     </div>
-  </div>
-
-</div>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: 'Menu',
-  data () {
-    return {
-    }
-  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import "./src/assets/colors.scss";
 
-button:focus {
-  outline:none;
-}
-
-a, a:visited, a:hover, a:active {
-  color: inherit;
-}
-
-.nav-link {
-    font-size:18px;
-    color: white!important;
-}
-
-.nav-link:hover {
-    color:$blue!important;
+.navbar {
+  font-family: 'Abel', sans-serif;
 }
 
 .navbar-brand {
-    font-size:24px;
-    color:white!important;
+  font-size: 24px;
 }
 
-.navbar {
-    font-family: 'Abel', sans-serif;
-}
-
-.navbar-toggler {
-  display:none;
-}
-
-@media (max-width: 575.98px) {
-  ul {
-    width:100%;
-    text-align:center;
-  }
-
-  .dissapear-small {
-    display:none !important;
-  }
-
-  .navbar-toggler {
-    display:inline-block;
+.navbar-dark .navbar-nav .nav-link {
+  font-size: 18px;
+  color: #fff;
+  &:hover {
+    color: $blue;
   }
 }
-
 </style>
