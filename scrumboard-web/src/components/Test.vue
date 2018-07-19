caf51daaff1786ed8f2006f65bc31df9f88f9794
Erwin Olie <erwinolie@gmail.com>
2018-07-19 13:12:18 +0200

    created beginning of a scrumboard

--- scrumboard-web/src/components/Test.vue
+++ scrumboard-web/src/components/Test.vue
@@ -4,7 +4,26 @@
     <TheHeader/>
 
     <div class="container">
-      hello world!
+      <div class="row flex-nowrap">
+
+        <div v-for="lane in lanes" :key="lane.title" class="col-6 p-2 bg-light border border-success">
+
+          <div class="row">
+            <div class="col-12">
+              <h3>{{ lane.title }}</h3>
+            </div>
+            <div v-for="item in lane.items" :key="item" class="col-12">
+              <div class="card m-2">
+                <div class="card-body">
+                  {{ item }}
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
 
   </div>
@@ -14,6 +33,40 @@
 import TheHeader from './shared/TheHeader'
 
 export default {
+  data () {
+    return {
+      lanes: [
+        {
+          title: 'Todo',
+          items: [
+            'item 1',
+            'item 2',
+            'item 7'
+          ]
+        },
+        {
+          title: 'Progress',
+          items: [
+            'item 3',
+            'item 4'
+          ]
+        },
+        {
+          title: 'Testing',
+          items: [
+            'item 5'
+          ]
+        },
+        {
+          title: 'Done',
+          items: [
+            'item 6',
+            'item 8'
+          ]
+        }
+      ]
+    }
+  },
   components: {
     TheHeader
   }
@@ -21,5 +74,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .row {
+    overflow-x: scroll;
+  }
 </style>
